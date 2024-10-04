8fd78c44f622d9d8905a6e5f982cba8c7005dab1
enepac <enrico.acha@gmail.com>
2024-10-03 21:51:31 -0400

    Create Contact Section

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -73,7 +73,13 @@ const EventPlanner = () => {
             </section>
             
             <section className="contact">
-
+            <h2>Contact Us</h2>
+                <form>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button className="submit-button">Send</button>
+                </form>
             </section>
         </div>
     );
